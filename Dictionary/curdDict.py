8c770c7aedbe0ce8a5abab705ca8f25de7b8ea8e
Diamond Dewan <diamonddewan@gmail.com>
2018-08-13 22:46:41 +0600

    Dictionary data type

---
+++ Dictionary/curdDict.py
@@ -0,0 +1,21 @@
+# Syntax
+
+'''
+dict = {key : value}
+'''
+
+shop_itmes = {
+    'rice': 50,
+    'flour': 40,
+    'milk': 30,
+    'oil': 57,
+
+}
+
+print(shop_itmes)
+
+# Add new item
+
+shop_itmes['fish'] = 200
+
+print(shop_itmes)
