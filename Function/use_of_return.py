368304db80259788a4b5af0de270748ef89980d7
Diamond Dewan <diamonddewan@gmail.com>
2018-08-24 20:32:01 +0600

    Function return

---
+++ Function/use_of_return.py
@@ -0,0 +1,17 @@
+'''
+in python and other programming language 
+use return keyword to return value
+from the function.
+'''
+
+
+def sum(x, y):
+    return x + y
+    print("This line won't print !!")
+
+
+print(sum(2, 3))
+
+'''
+Note: Any code after the return statement will never execute.
+'''
