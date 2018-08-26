69c3b6236a045bfd0e64a6db26c6f0a8346952f8
Diamond Dewan <diamonddewan@gmail.com>
2018-08-26 22:13:31 +0600

    Use of doc strings

---
+++ Function/doc_string.py
@@ -0,0 +1,18 @@
+'''
+This is a doc string
+'''
+
+'''
+The doc strings are used to describe how does code works
+basically used inside a function
+'''
+
+
+def hello(name):
+    '''
+    Say hello to you
+    '''
+    print('\nHello', name)
+
+
+hello(input('Enter your name: '))
